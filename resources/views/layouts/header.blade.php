ad2838c6703a9fd210233bfbf844e1a82d22c5f9
Karol <karol@uniqsoft.pl>
2024-02-21 21:33:21 +0100

    Konon

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,8 +1,8 @@
-<header class="bg-dark py-5">
+<header class="bg-conon py-5">
     <div class="container px-4 px-lg-5 my-5">
         <div class="text-center text-white">
-            <h1 class="display-4 fw-bolder">Shop in style</h1>
-            <p class="lead fw-normal text-white-50 mb-0">With this shop hompeage template</p>
+            <h1 class="display-4 fw-bolder">Najlepszy handlarz na świecie</h1>
+            <p class="lead fw-normal text-white-50 mb-0">Kupujemy tanio sprzedajemy drogo</p>
         </div>
     </div>
 </header>
